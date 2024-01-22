8878f9047212fa4db805adcfba6188381d0c678a
Sharon <Sharon@10.0.0.8>
2024-01-22 10:53:04 +0200

    triangle version 1

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest {
         String result = triangle.get_type().toString();
         assertEquals(expected, result.toString());
     }
-	//jkjk
+	//nisayon
 	@Test
 	public void check_Scalene(){
         int a = 2;
